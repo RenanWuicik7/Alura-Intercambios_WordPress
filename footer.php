d6b54b4c90bf681fad945a6a81fae5289c0ef8b9
Renan Cezar Wuicik <renan.wuicik.15@gmail.com>
2023-06-25 17:14:21 -0300

    Adicionando arquivo de construção do rodapé das páginas do site

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="container-alura">&copy; <?= date(format: "Y") ?> - Todos os direitos reservados à Alura Intercâmbios</p>
+</footer>
+
+</body>
+<?php wp_footer(); ?>
+</html>
